1a921a8db451f325b2bba76c281efb6ad4328f6d
Emily <emilyllewelyn@gmail.com>
2021-05-30 23:23:27 +0100

    restart, cleaned it up

--- script.js
+++ script.js
@@ -2,63 +2,49 @@ const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelectorAll('.clear');
 const numberButtons = document.querySelectorAll('.number');
 const equalsButton = document.querySelectorAll('.equals');
+const display = document.querySelector('.display');
 
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', function (e) {
-    var operator = e.target.id;
-    console.log(operator);
-  });
-});
+const buttonsData = [];
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', function (e) {
-    var number = e.target.id;
+    let number = e.target.id;
     console.log(number);
+    buttonsData.push(number);
+    console.log(buttonsData);
   });
 });
-
 equalsButton.forEach((button) => {
   button.addEventListener('click', function (e) {
-    var equals = e.target.id;
+    let equals = e.target.id;
     console.log(equals);
   });
 });
 
 clearButton.forEach((button) => {
   button.addEventListener('click', function (e) {
-    var clear = e.target.id;
+    let clear = e.target.id;
     console.log(clear);
   });
 });
 
-function operate(a, b, operator) {
-  switch (operator) {
-    case '+':
-      add(a, b);
-      break;
-    case '-':
-      subtract(a, b);
-      break;
-    case '/':
-      divide(a, b);
-      break;
-    case '*':
-      multiply(a, b);
-      break;
-  }
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    let operator = e.target.id;
+    console.log(operator);
+  });
+});
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
 }
 
-function add(a, b) {
-  return a + b;
-}
-function subtract(a, b) {
-  return a - b;
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
 }
 
-function divide(a, b) {
-  return a / b;
-}
-
-function multiply(a, b) {
-  return a * b;
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
 }

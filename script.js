ff1b2cf900d0a66877a0a3e052a2b4d8db4ff5f7
Emily <emilyllewelyn@gmail.com>
2021-05-31 22:52:27 +0100

    update for buttons functions

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelectorAll('.clear');
-const numberButtons = document.querySelectorAll('.number');
+const buttons = document.querySelectorAll('button');
 const equalsButton = document.querySelectorAll('.equals');
 const display = document.querySelector('.display');
 
 const buttonsData = [];
 
-numberButtons.forEach((button) => {
+buttons.forEach((button) => {
   button.addEventListener('click', function (e) {
-    let number = e.target.id;
-    console.log(number);
-    buttonsData.push(number);
-    console.log(buttonsData);
-  });
-});
-equalsButton.forEach((button) => {
-  button.addEventListener('click', function (e) {
-    let equals = e.target.id;
-    console.log(equals);
-  });
-});
-
-clearButton.forEach((button) => {
-  button.addEventListener('click', function (e) {
-    let clear = e.target.id;
-    console.log(clear);
+    if (e.target.id === 'number') {
+      let number = e.target.innerHTML;
+      console.log(number);
+      buttonsData.push(number);
+      console.log(buttonsData);
+    }
+    if (e.target.id === 'clear') {
+      buttonsData.splice(0, buttonsData.length);
+    }
+    if (e.target.id === 'operator') {
+      let operator = e.target.innerHTML;
+      buttonsData.push(operator);
+    }
+    display.innerHTML = buttonsData;
   });
 });
 
-operatorButtons.forEach((button) => {
-  button.addEventListener('click', function (e) {
-    let operator = e.target.id;
-    console.log(operator);
-  });
-});
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
 }

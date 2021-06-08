03a22a16e0ae637a8793c02b4f42f406346ee000
Emily <emilyllewelyn@gmail.com>
2021-06-08 10:24:51 +0100

    update

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ var colors = [
   'background: #BED3AC; color: #484a49',
   'background: #EAEBEF; color: #484a49',
 ];
-
+let count = 0;
+console.log(count + ' count from the top');
 //make the number buttons
 for (let i = 0; i < 10; i++) {
   const numberButtons = document.createElement('button');
@@ -17,6 +18,7 @@ for (let i = 0; i < 10; i++) {
   numberButtons.textContent = i;
   numberButtons.classList.add('number');
   numberButtons.setAttribute('id', i);
+  //change styling
   var random_color = colors[Math.floor(Math.random() * colors.length)];
   numberButtons.setAttribute('style', `${random_color}`);
 }
@@ -53,6 +55,7 @@ for (let i = 0; i < 6; i++) {
   }
   operators.appendChild(opButtons);
 }
+
 //add the event listeners to all buttons
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -67,6 +70,7 @@ buttons.forEach((button) => {
 
     if (e.target.classList == 'operator') {
       let o = button.id;
+      count++;
       operatorSet(o);
     }
 
@@ -76,22 +80,27 @@ buttons.forEach((button) => {
           element == '×' || element == '÷' || element == '+' || element == '-'
       );
       console.log(operator + 'is the op from equals function');
-      operate(operator);
+      operate(operator, count);
     }
   });
 });
 
 function operatorSet(o) {
   operator = o;
-  console.log(operator);
   buttonsData.push(operator);
   console.log(...buttonsData);
   display.textContent = buttonsData.join('');
+  //count the operator buttons
+
+  if (count >= 2) {
+    operate(operator, count);
+    console.log(count + 'count from inside if stm in operator set');
+  }
+  console.log(count + ' count from inside operatorSet');
 }
 
 function number(n) {
   var number = n;
-  console.log(number);
   buttonsData.push(number);
   console.log(...buttonsData);
   display.textContent = buttonsData.join('');
@@ -102,39 +111,73 @@ function clear() {
   display.textContent = '';
 }
 
-function operate(operator) {
-  let equalsArray = Array.from(buttonsData);
-  console.log(equalsArray);
+function operate(operator, count) {
+  if (count == 1) {
+    let equalsArray = Array.from(buttonsData);
+    console.log(equalsArray);
+    console.log(count + 'from inside the operate fn');
+    let opIndex = equalsArray.indexOf(operator);
+    console.log(opIndex + ' this is op index');
 
-  let opIndex = equalsArray.indexOf(operator);
-  console.log(opIndex + ' this is op index');
+    let a = equalsArray.slice(0, opIndex).join('');
+    console.log(a + "a works don't touch it");
+    let b = equalsArray.slice(opIndex + 1).join('');
+    console.log(b + 'this is b');
+    switch (operator) {
+      case '+':
+        plus(a, b);
+        console.log('the add switch works in count less than 1 ');
+        break;
+      case '-':
+        minus(a, b);
+        console.log('the minus switch works in count less than 1 ');
+        break;
+      case '×':
+        multiply(a, b);
+        console.log('the multiply switch works in count less than 1 ');
+        break;
+      case '÷':
+        divide(a, b);
+        console.log('the divide switch works in count less than 1 ');
+        break;
 
-  let a = equalsArray.slice(0, opIndex).join('');
-  console.log(a + "a works don't touch it");
-  let b = equalsArray.slice(opIndex + 1).join('');
-  console.log(b + 'this is b');
+      default:
+        console.log('broken?');
+        break;
+    }
+  } else if (count >= 2) {
+    let freshArray = Array.from(buttonsData);
+    console.log(freshArray);
+    console.log(count + ' is count from inside the goalong fn');
+    let opIndex = freshArray.indexOf(operator);
+    console.log(opIndex + ' this is op index from goalong fn');
 
-  switch (operator) {
-    case '+':
-      plus(a, b);
-      console.log('the add switch works');
-      break;
-    case '-':
-      minus(a, b);
-      console.log('the minus switch works');
-      break;
-    case '×':
-      multiply(a, b);
-      console.log('the multiply switch works');
-      break;
-    case '÷':
-      divide(a, b);
-      console.log('the divide switch works');
-      break;
+    let a = freshArray.slice(0, opIndex).join('');
+    console.log(a + "a works don't touch it");
+    let b = freshArray.slice(opIndex + 1, freshArray.length - 1).join('');
+    console.log(b + 'this is b');
+    switch (operator) {
+      case '+':
+        plus(a, b);
+        console.log('the add switch works in count less than 1 ');
+        break;
+      case '-':
+        minus(a, b);
+        console.log('the minus switch works in count less than 1 ');
+        break;
+      case '×':
+        multiply(a, b);
+        console.log('the multiply switch works in count less than 1 ');
+        break;
+      case '÷':
+        divide(a, b);
+        console.log('the divide switch works in count less than 1 ');
+        break;
 
-    default:
-      console.log('broken?');
-      break;
+      default:
+        console.log('broken?');
+        break;
+    }
   }
 }
 
